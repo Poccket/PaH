66114c72cb7fed51e786acb3f534f8394581fb23
poccket <ccrystal.cclods@gmail.com>
2018-11-21 15:14:46 -0600

    made the worst code ever

--- display.py
+++ display.py
@@ -23,24 +23,42 @@ def asBlock(text, width, height, heavyB):
 	while x > 0:
 		output += block[heavy][5]
 		x -= 1
-	output += block[heavy][1]+ '\n' + block[heavy][4]
+	output += block[heavy][1]+ '\n'
 	currHeight += 1
 	if text:
 		maxWidth = width - 2
 		counter = 0
+		if currHeight < height:
+			output += block[heavy][4]
 		for x in text:
 			if currHeight < height:
-				if counter < maxWidth:
+				if counter < maxWidth-1:
 					output += x
 					counter += 1
 				else:
 					output += block[heavy][4] + "\n" + block[heavy][4] + x
 					currHeight += 1
 					counter = 1
+		if currHeight < height:
+			while counter < maxWidth:
+				output += " "
+				counter += 1
+			output += block[heavy][4] + '\n'
+			currHeight += 1
+	while currHeight < height:
+		output += block[heavy][4]
+		counter = 0
 		while counter < maxWidth:
 			output += " "
 			counter += 1
-		output += block[heavy][4]
+		output += block[heavy][4] + '\n'
+		currHeight += 1
+	x = width - 2
+	output += block[heavy][2]
+	while x > 0:
+		output += block[heavy][5]
+		x -= 1
+	output += block[heavy][3]
 	print(currHeight)
 	return output
 
