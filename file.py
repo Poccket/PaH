e22c013761277320064d8e74bdc88388d8f42a46
Poccket <ccrystal.cclods@gmail.com>
2019-03-06 22:37:22 -0600

    Added basic comments

--- file.py
+++ file.py
@@ -6,13 +6,13 @@ def readAsList(fileName):
 	readAsList(str fileName)
 		Reads files into a list, splitting at newlines.
 	'''
-	fileOpen = open(fileName, "r")
-	return fileOpen.read().splitlines()
+	fileOpen = open(fileName, "r")								#	set fileOpen as the file, with option 'r' for reading.
+	return fileOpen.read().splitlines()							#	return an array version of the file, where it splits at newlines.
 
 def readAsRaw(fileName):
-	''' 
+	'''
 	readFileAsRaw(str fileName)
 		Reads files into a string, no splitting.
 	'''
-	fileOpen = open(fileName, "r")
-	return fileOpen.read()
+	fileOpen = open(fileName, "r")								#	set fileOpen as the file, with option 'r' for reading.
+	return fileOpen.read()										#	return a string version of the file, with no manipulation.
