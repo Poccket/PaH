28310286bfa82528ba378c2ebdade23c7325261f
Poccket <ccrystal.cclods@gmail.com>
2019-03-30 15:07:56 -0500

    Fixes for #1 and allowing varied decks.
    Changed some code to automagically work when decks aren't 52 cards long.
    Also fixed #1 with the new feature in modList.py

--- runWar.py
+++ runWar.py
@@ -7,7 +7,7 @@ input('Please enter full-screen, then press enter. This demo requires a terminal
 
 while True:
 	deck = modFile.read_list("cardFunkyFrench.txt")
-	available = list(range(52))
+	available = list(range(len(deck)))
 	x = None
 	turn = True
 	human_deck = []
@@ -15,7 +15,7 @@ while True:
 
 	while len(available) > 0:
 		while x not in available:
-			x = random.randrange(0, 52)
+			x = random.randrange(0, len(deck))
 		if turn:
 			human_deck.append(deck[x])
 		else:
@@ -25,32 +25,18 @@ while True:
 
 	deck_display_upper = []
 	deck_display_lower = []
-	for x in human_deck[:len(human_deck)//2]:
+
+	deck_half = len(human_deck) % 2
+
+	for x in human_deck[:(len(human_deck)//2)+deck_half]:
 		deck_display_upper.append(modDisp.as_block(x, aslist=True))
-	deck_display = modList.merge_alternate(deck_display_upper)
-	for x in human_deck[len(human_deck)//2:]:
+	deck_display = modList.merge_alternate(deck_display_upper, endchar="\n")
+	for x in human_deck[(len(human_deck)//2)+deck_half:]:
 		deck_display_lower.append(modDisp.as_block(x, aslist=True))
-	deck_display.extend(modList.merge_alternate(deck_display_lower))
+	deck_display.extend(modList.merge_alternate(deck_display_lower, endchar="\n"))
 	print("Your deck:")
-	y = 1
-	z = 11
-	a = 1
 	for x in deck_display:
-		if z == 11:
-			print(f'{a:02}', end='')
-			a += 1
-		else:
-			print('  ', end='')
-		if y == 13:
-			print(x)
-			y = 0
-			if z == 11:
-				z = 0
-			else:
-				z += 1
-		else:
-			print(x, end='')
-		y += 1
+		print(x, end='')
 	print('', flush=True)
 
 	select = input("Enter a number to select a card, or anything else to exit> ")
