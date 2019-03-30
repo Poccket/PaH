cdf300de37a2a56c5c45b9766765df6252537f76
Poccket <ccrystal.cclods@gmail.com>
2019-03-29 22:55:32 -0500

    Better merge function, allows up to 10 lists

--- list.py
+++ list.py
@@ -1,40 +1,43 @@
 # list.py
 #	functions for manipulating and creating lists.
 
-def merge_everyOther(listA, listB):
+def merge_everyOther(listA=[], listB=[], listC=[], listD=[], listE=[], listF=[], listG=[], listH=[], listI=[], listJ=[], **kwargs):
 	'''
 	Merges two lists into a single list
 
 	Merges two lists, taking the first of each and inserting it into a new list, then the second of each, and so on.
 
 	Parameters:
-	listA		(list)	:	The first list, will be the first of each set
-	listB		(list)	:	The second list, will be the second of each set
+	listA-J		(list)	:	The set of list, maximum being 10, minimum being 2.
 	Returns:
 	list				:	A merged list
 	'''
 	output = []
-
-	if len(listA) >= len(listB):
-		lenSmall = len(listB)
-	else:
-		lenSmall = len(listA)
-
-	print(str(lenSmall) + " " + str(len(listA)) + " " + str(len(listB)))
+	length = 0
 	x = 0
-	while x < lenSmall:
-		print(x)
-		output.append(listA[x])
-		output.append(listB[x])
-		x += 1
 
-	if len(listA) > len(listB):
-		while x < len(listA):
-			output.append(listA[x])
-			x += 1
-	elif len(listB) > len(listA):
-		while x < len(listB):
-			output.append(listB[x])
-			x += 1
+	if len(listA) > length: length = len(listA)
+	if len(listB) > length: length = len(listB)
+	if len(listC) > length: length = len(listC)
+	if len(listD) > length: length = len(listD)
+	if len(listE) > length: length = len(listE)
+	if len(listF) > length: length = len(listF)
+	if len(listG) > length: length = len(listG)
+	if len(listH) > length: length = len(listH)
+	if len(listI) > length: length = len(listI)
+	if len(listJ) > length: length = len(listJ)
+
+	while x < length:
+		if len(listA) > x: output.append(listA[x])
+		if len(listB) > x: output.append(listB[x])
+		if len(listC) > x: output.append(listC[x])
+		if len(listD) > x: output.append(listD[x])
+		if len(listE) > x: output.append(listE[x])
+		if len(listF) > x: output.append(listF[x])
+		if len(listG) > x: output.append(listG[x])
+		if len(listH) > x: output.append(listH[x])
+		if len(listI) > x: output.append(listI[x])
+		if len(listJ) > x: output.append(listJ[x])
+		x += 1
 
 	return output
