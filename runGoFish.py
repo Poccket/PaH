329302647f43b964523a52c53efc6af14b2a2cf9
Poccket <ccrystal.cclods@gmail.com>
2019-04-05 21:18:48 -0500

    Major improvements and fixes to runGoFish.py
    "Chat System" implemented, which makes the chat a lot less messy, and fixes the issue where the hand displayed would overwrite it.
    Made it to where finishing the game didn't break the nice display by re-printing the cards before ending the game.
    Right before the game ends, your deck is changed to be your matches! See all 2 of the matches you got before the robot ran you out of cards!
    Other minor changes

--- runGoFish.py
+++ runGoFish.py
@@ -3,6 +3,7 @@ import modFile
 import modList
 import modHelper
 import modUI
+import modPlayers
 import random
 import math
 import time
@@ -15,6 +16,17 @@ print("Make sure your terminal size is large enough, or you'll get an error!")
 print("---")
 
 ui = modUI.Screen()
+human_player = modPlayers.Player("Player", {'hand': [], 'matches': []})
+robot_player = modPlayers.Player("Robot", {'hand': [], 'matches': []})
+system = modPlayers.Player("System", groups=['system'])
+
+messages = []
+chat_height = 0
+curr_height = 0
+
+deck = modFile.read_list("decks/cardFrench.txt")
+available = list(range(len(deck)))
+turn = True
 
 suits = {
 	"colors": {
@@ -58,13 +70,6 @@ def rankcheck(inp):
 	return "Error"
 
 
-deck = modFile.read_list("decks/cardFrench.txt")
-available = list(range(len(deck)))
-turn = True
-human_hand = []
-robot_hand = []
-
-
 def dealcard():
 	if len(deck) != 0:
 		card = None
@@ -76,74 +81,44 @@ def dealcard():
 	return card
 
 
-while len(available) > 44:
-	if turn:
-		human_hand.append(deck[dealcard()])
-	else:
-		robot_hand.append(deck[dealcard()])
-	turn = not turn
-
-human_matches = []
-robot_matches = []
-human_match_count = math.ceil(len(human_matches) / 2)
-robot_match_count = math.ceil(len(robot_matches) / 2)
-
-
-def update_scores():
-	global human_match_count
-	global robot_match_count
-	human_match_count = math.ceil(len(human_matches) / 2)
-	robot_match_count = math.ceil(len(robot_matches) / 2)
+def send(usr, msg):
+	global chat_height
+	messages.append("<" + usr.name + "> " + msg)
+	if len(messages) > 20:
+		del messages[0]
+	for a in range(chat_height, ui.height):
+		ui.line(chat_height, "clear")
+	for msg in messages:
+		ui.line(chat_height, "insert", msg)
+	ui.print()
 
 
-while True:
-	update_scores()
+def printhand():
+	global curr_height
+	global chat_height
+
+	# -- UI Variables --
+	# The amount of cards that can fit on one row
 	max_cards = math.floor(ui.width / 14)
-	rows_needed = math.ceil(len(human_hand) / max_cards)
-	ui_height = math.ceil(rows_needed * 12) + 3
-	curr_height = ui_height-4
-	ui.line(ui_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
-									.format(robot_match_count, human_match_count, len(available)))
-
-	if len(human_hand) < 1:
-		ui.line(ui_height, "insert", "Human hand exhausted! Ending game.", True)
-		break
-	if len(robot_hand) < 1:
-		ui.line(ui_height, "insert", "Robot hand exhausted! Ending game.", True)
-		break
-
-	for c1 in human_hand:
-		for c2 in human_hand:
-			if c1 != c2 and rankcheck(c1) == rankcheck(c2) and colorcheck(c1) == colorcheck(c2):
-				ui.line(ui_height, "insert", "Player: Match in my hand.", True)
-				human_matches.extend((c1, c2))
-				human_hand = [e for e in human_hand if e not in (c1, c2)]
-				update_scores()
-				ui.line(ui_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
-												.format(robot_match_count, human_match_count, len(available)))
-				ui.line(ui_height, "insert", "You have {} win(s)!".format(human_match_count), True)
-	for c1 in robot_hand:
-		for c2 in robot_hand:
-			if c1 != c2 and rankcheck(c1) == rankcheck(c2) and colorcheck(c1) == colorcheck(c2):
-				ui.line(ui_height, "insert", "Robot: Got a match here!", True)
-				robot_matches.extend((c1, c2))
-				robot_hand = [e for e in robot_hand if e not in (c1, c2)]
-				update_scores()
-				ui.line(ui_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
-												.format(robot_match_count, human_match_count, len(available)))
+	# The amount of rows needed to display all of the player's cards
+	rows_needed = math.ceil(len(human_player.hands['hand']) / max_cards)
+	# The height that the cards will start printing at.
+	curr_height = math.ceil(rows_needed * 12)-1
+	# Exactly 4 rows above the cards (Where chat is printed)
+	chat_height = math.ceil(rows_needed * 12) + 3
 
 	hand_blocks = []
-	for x in human_hand:
-		hand_blocks.append(modDisp.as_block(x, aslist=True))
+	for i in human_player.hands['hand']:
+		hand_blocks.append(modDisp.as_block(i, aslist=True))
 
 	hand_display = modList.merge_alternate(hand_blocks, endchar="list")
 
-	row_startingCard = 0
+	startrow = 0
 
 	curr_line = ""
-	for x in range(0, rows_needed):
+	for i in range(0, rows_needed):
 		for z in hand_display:
-			for y in range(row_startingCard, max_cards+row_startingCard):
+			for y in range(startrow, max_cards+startrow):
 				if y < len(z):
 					if z == hand_display[0]:
 						curr_line += f'{y:02}'
@@ -153,84 +128,145 @@ while True:
 			ui.line(curr_height, "change", curr_line)
 			curr_height -= 1
 			curr_line = ""
-		row_startingCard += max_cards
+		startrow += max_cards
 	ui.print()
+
+
+while len(available) > 44:
+	if turn:
+		human_player.hands['hand'].append(deck[dealcard()])
+	else:
+		robot_player.hands['hand'].append(deck[dealcard()])
+	turn = not turn
+
+human_match_count = 0
+robot_match_count = 0
+
+
+def update_scores():
+	global human_match_count
+	global robot_match_count
+	human_match_count = math.ceil(len(human_player.hands['matches']) / 2)
+	robot_match_count = math.ceil(len(robot_player.hands['matches']) / 2)
+
+
+while True:
+	# -- Win conditions --
+	# If human player's hand is empty
+	if len(human_player.hands['hand']) < 1:
+		send(system, "Human hand exhausted! Ending game.")
+		human_player.hands['hand'] = human_player.hands['matches']
+		printhand()
+		break
+	# If robot player's hand is empty
+	if len(robot_player.hands['hand']) < 1:
+		send(system, "Robot hand exhausted! Ending game.")
+		human_player.hands['hand'] = human_player.hands['matches']
+		printhand()
+		break
+	# If deck is empty
+	if len(available) < 0:
+		send(system, "Deck exhausted! Ending game.")
+		human_player.hands['hand'] = human_player.hands['matches']
+		printhand()
+		break
+
+	# -- Match checking --
+	# Check for matches in the player's hand.
+	for c1 in human_player.hands['hand']:
+		for c2 in human_player.hands['hand']:
+			if c1 != c2 and rankcheck(c1) == rankcheck(c2) and colorcheck(c1) == colorcheck(c2):
+				send(human_player, "Match in my hand.")
+				human_player.hands['matches'].extend((c1, c2))
+				human_player.hands['hand'] = [e for e in human_player.hands['hand'] if e not in (c1, c2)]
+				update_scores()
+				ui.line(chat_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
+												.format(robot_match_count, human_match_count, len(available)))
+	# Check for matches in the robot's hand.
+	for c1 in robot_player.hands['hand']:
+		for c2 in robot_player.hands['hand']:
+			if c1 != c2 and rankcheck(c1) == rankcheck(c2) and colorcheck(c1) == colorcheck(c2):
+				send(robot_player, "Got a match here!")
+				robot_player.hands['matches'].extend((c1, c2))
+				robot_player.hands['hand'] = [e for e in robot_player.hands['hand'] if e not in (c1, c2)]
+				update_scores()
+				ui.line(chat_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
+												.format(robot_match_count, human_match_count, len(available)))
+
+	printhand()
+	# Update scores
+	update_scores()
+	ui.line(chat_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
+									.format(robot_match_count, human_match_count, len(available)))
+
 	gofish = True
 	if turn:
 		select = "Not a number!"
-		while not modHelper.isinteger(select) or int(select) > len(human_hand)-1:
+		while not modHelper.isinteger(select) or int(select) > len(human_player.hands['hand'])-1:
 			select = input("Pick a card to play> ")
-			if modHelper.isinteger(select) and int(select) > len(human_hand)-1:
+			if modHelper.isinteger(select) and int(select) > len(human_player.hands['hand'])-1:
 				print("That number is too big!")
 			if not modHelper.isinteger(select):
 				print("Need to input a number!")
 		select = int(select)
-		ui.line(ui_height, "insert", "Player: Do you have a {} {}?"
-										.format(colorcheck(human_hand[select]), rankcheck(human_hand[select])), True)
+		send(human_player, "Do you have a {} {}?"
+										.format(colorcheck(human_player.hands['hand'][select]), rankcheck(human_player.hands['hand'][select])))
 
-		for x in robot_hand:
-			if rankcheck(x) == rankcheck(human_hand[select]) and colorcheck(x) == colorcheck(human_hand[select]):
+		for x in robot_player.hands['hand']:
+			if rankcheck(x) == rankcheck(human_player.hands['hand'][select]) and colorcheck(x) == colorcheck(human_player.hands['hand'][select]):
 				gofish = False
-				human_matches.extend((x, human_hand[select]))
-				robot_hand = [e for e in robot_hand if e not in (x, '')]
-				human_hand = [e for e in human_hand if e not in (human_hand[select])]
+				human_player.hands['matches'].extend((x, human_player.hands['hand'][select]))
+				robot_player.hands['hand'] = [e for e in robot_player.hands['hand'] if e not in (x, '')]
+				human_player.hands['hand'] = [e for e in human_player.hands['hand'] if e not in (human_player.hands['hand'][select])]
 				time.sleep(1)
-				ui.line(ui_height, "insert", "Robot: Yeah, here you go.", True)
+				send(robot_player, "Yeah, here you go.")
 				time.sleep(1)
 				update_scores()
-				ui.line(ui_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
+				ui.line(chat_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
 												.format(robot_match_count, human_match_count, len(available)))
-				ui.line(ui_height, "insert", "You have {} win(s)!".format(human_match_count), True)
 				break
 
 		if gofish:
 			time.sleep(1)
-			ui.line(ui_height, "insert", "Robot: Sorry, I don't. Go fish.", True)
+			send(robot_player, "Sorry, I don't. Go fish.")
 			card_in = dealcard()
-			if card_in:
-				human_hand.append(deck[card_in])
-			else:
-				ui.line(ui_height, "insert", "Deck is empty! Ending game.", True)
-				break
+			if card_in is not False:
+				human_player.hands['hand'].append(deck[card_in])
 
 		turn = not turn
 	else:
-		select = random.randrange(0, len(robot_hand))
+		select = random.randrange(0, len(robot_player.hands['hand']))
 		time.sleep(2)
-		ui.line(ui_height, "insert", "Robot: Got a {} {}?"
-										.format(colorcheck(robot_hand[select]), rankcheck(robot_hand[select])), True)
+		send(robot_player, "Got a {} {}?"
+							.format(colorcheck(robot_player.hands['hand'][select]), rankcheck(robot_player.hands['hand'][select])))
 		gofish = True
-		for x in human_hand:
-			if rankcheck(x) == rankcheck(robot_hand[select]) and colorcheck(x) == colorcheck(robot_hand[select]):
+		for x in human_player.hands['hand']:
+			if rankcheck(x) == rankcheck(robot_player.hands['hand'][select]) and colorcheck(x) == colorcheck(robot_player.hands['hand'][select]):
 				gofish = False
-				robot_matches.extend((x, robot_hand[select]))
-				robot_hand = [e for e in robot_hand if e not in (robot_hand[select])]
-				human_hand = [e for e in human_hand if e not in (x, '')]
+				robot_player.hands['matches'].extend((x, robot_player.hands['hand'][select]))
+				robot_player.hands['hand'] = [e for e in robot_player.hands['hand'] if e not in (robot_player.hands['hand'][select])]
+				human_player.hands['hand'] = [e for e in human_player.hands['hand'] if e not in (x, '')]
 				time.sleep(1)
-				ui.line(ui_height, "insert", "Player: Yup, here you go.", True)
+				send(human_player, "Yup, here you go.")
 				time.sleep(1)
 				update_scores()
-				ui.line(ui_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
+				ui.line(chat_height-2, "change", "Wins: {} ROB - YOU {}    Deck: {} cards"
 												.format(robot_match_count, human_match_count, len(available)))
 				time.sleep(2)
 				break
 		if gofish:
 			time.sleep(1)
-			ui.line(ui_height, "insert", "Player: Don't have it, Go fish.", True)
+			send(human_player, "Don't have it, Go fish.")
 			card_in = dealcard()
-			if card_in:
-				robot_hand.append(deck[card_in])
-			else:
-				ui.line(ui_height, "insert", "Deck is empty! Ending game.", True)
-				break
+			if card_in is not False:
+				robot_player.hands['hand'].append(deck[card_in])
 
 		turn = not turn
 
-update_scores()
 if human_match_count == robot_match_count:
-	ui.line(ui_height, "insert", "There was a tie!", True)
+	send(system, "There was a tie!")
 elif human_match_count > robot_match_count:
-	ui.line(ui_height, "insert", "You won! Congratulations!", True)
+	send(system, "You won! Congratulations!")
 elif robot_match_count > human_match_count:
-	ui.line(ui_height, "insert", "You lost! Try again!", True)
+	send(system, "You lost! Try again!")
 ui.print()
