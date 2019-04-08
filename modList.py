6c1da61875900366fb90e72d7f1dc29bb45f72c8
Poccket <ccrystal.cclods@gmail.com>
2019-04-08 12:00:44 -0500

    Updated merge_alternate()
    modFile now follows guideline, as well as this, merge_alternate() is now improved.
    Previously there was a weird thing you could come across where you could get a list of lists, or a single list, and it wasn't completely predictable.
    Now you can select either of these, and the 'endchar' option is available in both of these modes.
    I also removed the check to see if the input list is actually a list, because why the hell not?

--- modList.py
+++ modList.py
@@ -1,36 +1,52 @@
-# list_manip.py
-# functions for manipulating and creating lists.
+"""Functions for manipulating or creating lists"""
 from typing import List
 
 
-def merge_alternate(listlist: list, endchar: str = None) -> List[str]:
-    """
-    Merges lists into one list.
+def merge_alternate(inp: list, mode: str, endchar: str = None) -> List[str]:
+    """Merges lists into one list using an alternating pattern.
 
-    Creates a single list from a set of lists by alternating through the lists.
-    For example, Item from A, Item from B, Item from C, Item from A and so on.
+    There are two modes able to be used in this function, detailed below.
+    To select a mode, set 'mode' to the first letter of the mode you wish to use.
 
-    :param listlist: A list of the lists to merge.
-    :param endchar: Character to put at the end of each iteration
+    m(ulti)
+        Creates a list of lists, from a set of lists.
+
+        For example:
+        List 0: Item 0 from A, Item 0 from B, Item 0 from C, endchar (if set)
+        List 1: Item 1 from A, Item 1 from B, and so on.
+
+    s(ingle)
+        Creates a single list, using every item from the lists.
+
+        For example:
+        Item 0 from A, Item 0 from B, Item 0 from C, endchar (if set), Item 1 from A, and so on.
+
+    :param inp: A list of the lists to merge.
+    :param mode: The mode you want to use.
+    :param endchar: Character to put at the end of each iteration, if set
     :return: The merged lists as a single list
     """
-    if type(listlist) != list:
-        raise TypeError("item 'listList' must be a list")
     output = []
-    length = len(max(listlist, key=len))
+    length = len(max(inp, key=len))
+    if not isinstance(mode, str):
+        raise TypeError("argument 'mode' must be str")
+    mode = mode.lower()
+    if mode not in ["s", "m"]:
+        raise ValueError("invalid mode: '" + mode + "'")
 
-    if endchar != "list":
+    if mode == 's':
         for vItem in range(0, length):
-            for vList in listlist:
-                if vItem <= len(vList)-1:
+            for vList in inp:
+                if vItem < len(vList):
                     output.append(vList[vItem])
             if endchar is not None:
                 output.append(endchar)
-    else:
+    elif mode == 'm':
         for vItem in range(0, length):
             output.append([])
-            for vList in listlist:
+            for vList in inp:
                 if vItem <= len(vList)-1:
                     output[vItem].append(vList[vItem])
-
+            if endchar is not None:
+                output[vItem].append(endchar)
     return output
