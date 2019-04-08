41770f0b0ad7fc559302986fba6a12a174cdd9d1
Poccket <ccrystal.cclods@gmail.com>
2019-04-08 11:01:04 -0500

    modHelper updated to have more documentation
    This means I added a docstring to the module, as well as commenting the reasons for why I coded the only function currently in there like I did.
    As well as this, I changed the input variable to 'inp' to be consistent with the changes I'm making to other modules.

--- modHelper.py
+++ modHelper.py
@@ -1,12 +1,21 @@
-def isinteger(i):
-    """
-    Checks if an object is an integer
+"""Helper functions"""
 
-    :param i: Input object
+
+def is_int(inp) -> bool:
+    """Checks if an object is an integer
+
+    :param inp: Input object
     :return: boolean
     """
     try:
-        int(i)
+        int(inp)
         return True
     except ValueError:
         return False
+    # Simply a helper function that checks if 'inp' can be run through 'int()'
+
+    # Imagine we have a, which is "1", making it a string. This means 'int(a)' would work, however, if we did something
+    # like 'return type(a) is int' wouldn't, despite the fact that it could work as an int.
+    # An example use case of this would be checking the value of a variable set with 'input()', since in Python 3 the
+    # output of 'input()' is always a string. So you could use this to break a while loop only when the variable is a
+    # something that works as an integer.
