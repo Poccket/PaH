7d21d63927bbad8e3605debb77ebc62df4bc8adc
Poccket <ccrystal.cclods@gmail.com>
2019-04-09 02:26:51 -0500

    Updated to support NT
    This now works on windows, which is great since that's where I'm usually developing.
    Sadly, PyCharm is always gonna complain about this file no matter where I am, since half is NT and half is Unix

--- modGetch.py
+++ modGetch.py
@@ -1,4 +1,17 @@
-def get_arrow():
+import os
+
+
+class _Arrow:
+    def __init__(self):
+        if os.name == 'nt':
+            self.impl = _arrownt
+        else:
+            self.impl = _arrowunix
+
+    def __call__(self): return self.impl()
+
+
+def _arrowunix():
     a, b, c = None, None, None
     keys = {'A': 'up', 'B': 'down', 'C': 'right', 'D': 'left', 'R': 'select', 'q': 'escape'}
     while c not in ['A', 'B', 'C', 'D']:
@@ -10,9 +23,46 @@ def get_arrow():
         if a == 'q':
             c = 'q'
             break
-        b = getch()
+        getch()
         c = getch()
     return keys[c]
+
+
+def _arrownt():
+    a, b = None, None
+    keys = {b'H': 'up', b'P': 'down', b'M': 'right', b'K': 'left', 'R': 'select', 'q': 'escape'}
+    while b not in [b'H', b'P', b'M', b'K']:
+        while a not in [b'\xe0', b'\r', b'q']:
+            a = getch()
+        if a == b'\r':
+            b = 'R'
+            break
+        if a == b'q':
+            b = 'q'
+            break
+        b = getch()
+    return keys[b]
+
+
+#class _ArrowNT:
+#    def __init__(self):
+#        pass
+#
+#    def __call__(self):
+#        a, b = None, None
+#        keys = {'H': 'up', 'P': 'down', 'M': 'right', 'K': 'left', 'R': 'select', 'q': 'escape'}
+#        while b not in [b'H', b'P', b'M', b'K']:
+#            while a not in [b'\xe0', b'\r', b'q']:
+#                a = getch()
+#            if a == b'\r':
+#                b = 'R'
+#                break
+#            if a == b'q':
+#                b = 'q'
+#                break
+#            b = getch()
+#        return keys[b]
+
 
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the
@@ -52,3 +102,4 @@ class _GetchWindows:
 
 
 getch = _Getch()
+get_arrow = _Arrow()
