0066cf48931210ad184bfed5c34a4932c0222c4b
Poccket <ccrystal.cclods@gmail.com>
2019-04-15 12:56:39 -0500

    More colors.

--- modUI.py
+++ modUI.py
@@ -4,11 +4,21 @@ import shutil
 import os
 
 colors = {
-    "RED"   : "\033[1;31m",
-    "GREEN" : "\033[1;32m",
-    "BLUE"  : "\033[1;34m",
-    "WHITE" : "\033[1;97m",
-    "RESET" : "\033[0;0m",
+    "RED":          "\033[31m",
+    "LIGHTRED":     "\033[1;31m",
+    "GREEN":        "\033[32m",
+    "LIGHTGREEN":   "\033[1;32m",
+    "YELLOW":       "\033[33m",
+    "LIGHTYELLOW":  "\033[1;33m",
+    "BLUE":         "\033[34m",
+    "LIGHTBLUE":    "\033[1;34m",
+    "MAGENTA":      "\033[35m",
+    "LIGHTMAGENTA": "\033[1;35m",
+    "CYAN":         "\033[36m",
+    "LIGHTCYAN":    "\033[1;36m",
+    "WHITE":        "\033[97m",
+    "LIGHTWHITE":   "\033[1;97m",
+    "RESET":        "\033[0;0m",
 }
 
 
